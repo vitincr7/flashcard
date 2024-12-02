51a460f06fbbc4a5c21ac87aa010643a402285ce
vyttor <00001116292786sp@al.educacao.sp.gov.br>
2024-12-02 09:16:24 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'cr7',
+    ' Quantos gols tem cr7?',
+    '915 gols'
+)
+
+criaCartao(
+    'cr7',
+    'Quantos anos ele tem?',
+    '39 anos'
+)
+
+criaCartao(
+    'cr7',
+    'Qual sua posiçao?',
+    'atacante'
+)
+
+criaCartao(
+    'cr7',
+    'qual foi seu primeiro time no profissional?',
+    'sporting'
+)
